453ae097c161f6227e761749830b5f3831547c1e
skalenoff <60006998+skalenoff@users.noreply.github.com>
2021-01-19 17:17:26 +0300

    Добавил первые запросы

--- my-app/src/app/pages/pricelist/pricelist.component.ts
+++ my-app/src/app/pages/pricelist/pricelist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators/map';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pricelist',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricelistComponent implements OnInit {
 
-  constructor() { }
+
+  url = '/fruits';
+  fruit!: Fruit[];
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  _getFruits(){
+    this.getFruits().subscribe(data => {
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getFruits(){
+    //const endpoint = '/fruits';
+    const endpoint = this.url;
+    console.log("GET, " + endpoint)
+    return this.http.get(endpoint, { headers: new HttpHeaders( {'Content-Type': 'application/json; charset=UTF-8'} )})
+    .pipe(
+      map((res:any) => {
+      this.fruit = res;
+      console.log("Загружено чтто-то");
+      console.log(this.fruit);
+      return true; })
+      );
+  }
+
+  _postFruits(){
+    this.postFruits().subscribe(data => {
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  postFruits(){
+    //const endpoint = '/fruits';
+    const endpoint = this.url;
+    console.log("GET, " + endpoint)
+
+    const request = {
+      description: "Hellow Quarkus!",
+      name: "Angular"
+    };
+
+    return this.http.post(endpoint, request, { headers: new HttpHeaders( {'Content-Type': 'application/json; charset=UTF-8'} )})
+    .pipe(
+      map((res:any) => {
+      this.fruit = res;
+      console.log("Загружено чтто-то");
+      console.log(this.fruit);
+      return true; })
+      );
+  }
+}
+
+export interface Fruit {
+  description: string;
+  name: string;
 }
